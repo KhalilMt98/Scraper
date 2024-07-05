1dc30824b0f1d748a4fa721f3cdbd08606f6b9ef
Khalil Mouteirek <khalil.mouteirek@gmail.com>
2024-07-06 01:55:31 +0300

    imported required packages

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
 function scraper(searchTerm) {
     const url = `https://www.linkedin.com/jobs/search/?keywords=${encodeURIComponent(searchTerm)}`;
 }
